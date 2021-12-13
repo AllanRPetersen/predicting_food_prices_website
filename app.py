336426808190b350bf258b2fd58ef38bf38258da
Allan Petersen <allanrp03@gmail.com>
2021-12-13 13:07:30 -0600

    Updated with datacleaning

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import requests
+import pandas as pd
+import numpy as np
 
 st.markdown('# Food Prediction API')
 
@@ -24,9 +26,88 @@ st.image(
 
 # Section 1:  Explaining the hypothesis of the project
 
+#Hypothesis:
+st.markdown('# Project Aim')
+st.write(
+    '''The aim of this project is to predict the price of food based on their nutrients.
+    Our hypothesis was that food containing more nutrients would be more desirable,
+    leading to increased demand and as a result being more expensive.
+    In contrast foods having few nutrients would have a lower price.''')
+
 ##############################################################################
 
 # Section 2:  Explaining the data of the project
+st.markdown('# Data')
+st.write(
+    '''Price of the foods were found as a dataset called Global Food Prices Database on Kaggle.
+This dataset contained over 1 million rows of food prices from different markets around the world.'''
+)
+st.write('The raw dataset is displayed below:')
+
+
+#@st.cache
+def get_dataframe_data():
+
+    return pd.read_csv(
+        'raw_data/Global Food Prices Database/wfp_food_prices_database.csv')
+
+
+price_df = get_dataframe_data()
+
+st.write(price_df.head())
+st.write(f'Shape of dataset: {price_df.shape}')
+
+st.markdown('## Data Cleaning')
+
+st.write(
+    '''We decide to use the name of the food to search for the nutritional information in the API (api name?).
+            To get a list of foods to search for we split the string in column cm_name (commitidy name)
+            using a lamda function, generating a new column: type ''')
+
+price_df['type'] = price_df['cm_name'].apply(lambda x: x.split('-')[0])
+st.write(price_df.head(3))
+
+price_2020_df = price_df[price_df['mp_year'] == 2020]
+
+st.write('''We aim to train our with the price of food in 2020.
+         Hence, the dataframe was filtered by the year 2020 using Boolean logic.'''
+         )
+
+st.write(
+    '''The dataset did not have that many missing values but required extensive cleaning before it could be used for modelling.
+    The last column (mp_commoditysource) was empty and as dropped. We no longer needed cm_name.
+    The column with the market name (adm1_name) contained 30% empty values. But was kept for now.
+    All the id columns did not provide any useful information for our model so they were dropped.
+    ''')
+st.write(
+    '''For the majority of the foods, the prices were given per KG or per L.
+         To avoid lengthy conversions we filtered the data to only include these entries.'''
+)
+
+price_2020_df.drop(columns=[
+    'adm0_id', 'adm1_id', 'mkt_id', 'cm_id', 'cur_id', 'cm_name', 'pt_id',
+    'mp_commoditysource', 'um_id'
+],
+                   inplace=True)
+
+price_2020_df = price_2020_df.loc[price_2020_df['um_name'].isin(['KG', 'L'])]
+
+st.write('We now had a dataframe that looked like this:')
+
+st.write(price_2020_df.head(3))
+
+st.markdown('## Currency')
+
+st.write('''The food price was given in the local currency.
+         In total there were 62 currencies which we needed to convert to a single currency
+         in order to compare the prices. The list of currencies were:  ''')
+
+st.write(price_2020_df['cur_name'].unique())
+
+st.write('''Each individual currency has a three letter abbreviation.
+         We passed a list of the abbreviations to an API called currencylayer
+         and it returned us a list of conversion rates to US dollars as a JSON.
+         This allowed us to convert the prices to US dollars.''')
 
 ##############################################################################
 
