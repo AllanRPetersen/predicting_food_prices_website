a2d0cb78551a334d27a5f88736bb292f84f098d9
Allan Petersen <allanrp03@gmail.com>
2021-12-17 10:49:34 -0600

    smaller csv files and some other updates

--- pages/page1.py
+++ pages/page1.py
@@ -5,27 +5,36 @@ import pandas as pd
 import numpy as np
 #from tensorflow.keras import models
 import joblib
-from utils import nutrients_super, nutrients_redux,nutrients_redux_2
+from utils import nutrients_super, nutrients_redux, nutrients_redux_2
 ###############################################################
-
 
 
 def app():
     st.markdown('## Predict if the food will be expensive or not')
 
-
-
-    food = st.text_input('put the food you want to calculate:')
+    food = st.text_input('put the food you want to calculate:', 'Tacos')
 
     result_2 = nutrients_super(food)
     dict1 = {}
-    for k,v in result_2.items():
+    for k, v in result_2.items():
         dict1[k] = [v]
     result_1 = pd.DataFrame.from_dict(dict1).replace('not found', np.nan)
 
     st.write(result_2)
 
-    cpi = pd.read_csv('raw_data/country_cpi.csv')
+    api_key = 'HbaYhOLNuzBDKvfs0qvVEB4Ymu1PxQmru9YdXvv2jfc'
+    query = food
+    response = requests.get(
+        f'https://api.unsplash.com/search/photos?client_id={api_key}&query={query}'
+    )
+    image_small = response.json()['results'][0]['urls']['small']
+    st.image(
+        image_small,
+        caption=
+        f' {response.json()["results"][0]["alt_description"]} by {response.json()["results"][0]["user"]["name"]}'
+    )
+
+    cpi = pd.read_csv('production_data/country_cpi.csv')
 
     country = st.selectbox("Country", cpi)
 
@@ -45,9 +54,10 @@ def app():
             'index_2019': answer
         }
 
-        st.write(answer)
+        #st.write(answer)
 
-        prediction = requests.get('https://nnmodel-ynawzkr5xa-ew.a.run.app/predict/', params=params)
+        prediction = requests.get(
+            'https://nnmodel-ynawzkr5xa-ew.a.run.app/predict/', params=params)
 
         st.write('Probability: ', prediction.json())
 
